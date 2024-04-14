13ea758ae8f434aa7fbb3255cc6fe93dd49bce28
Witaya252525 <witaya.c@kkumail.com>
2024-04-14 14:16:09 +0700

    First react

---
+++ src/TattooItems.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+
+function TattooItems() {
+  return (
+    <div className="tattoo-item">
+    <img src='/images/tattoo-01-small.jpg'/>
+    <h4>Witaya</h4>  
+  </div>
+  )
+}
+
+export default TattooItems;
